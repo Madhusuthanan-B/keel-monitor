d7b52c841a653130bcc7b3753087df498b2d2627
Madhusuthanan-B <madhub081011@gmail.com>
2021-06-13 22:13:04 +0530

    cog - getting data using observables, new approach

--- keel-monitor-app/src/app/simulators/base-simulator.ts
+++ keel-monitor-app/src/app/simulators/base-simulator.ts
@@ -1,6 +1,58 @@
+import { Observable, Subscription } from 'rxjs';
+import { Range } from '../models/range';
+
 export abstract class Simulator {
     abstract startSimulation(): void;
     abstract stopSimulation(): void;
+    private subscriptions: Map<string, Subscription> = new Map();
+
+    public rangeTicker(range: Range, currentValue: number): Observable<number> {
+        return new Observable((observer) => {
+
+            function forwardClock() {
+                const forward = setInterval(() => {
+                    if (currentValue <= range.end) {
+                        currentValue += range.delta;
+                        observer.next(currentValue);
+                    } else {
+                        clearInterval(forward);
+                        backwardClock()
+                    }
+                }, range.frequency);
+            }
+
+            function backwardClock() {
+                const backward = setInterval(() => {
+                    if (currentValue >= range.start) {
+                        currentValue -= range.delta;
+                        observer.next(currentValue);
+                    } else {
+                        clearInterval(backward);
+                        forwardClock();
+                    }
+                }, range.frequency);
+            }
+
+            forwardClock();
+        });
+    }
+
+    public addSubscription(key: string, value: Subscription) {
+        this.subscriptions.set(key, value);
+    }
+
+    public clearAllSubscriptions() {
+        this.subscriptions.forEach((value: Subscription, key: string) => {
+            this.removeSubscription(key);
+        });
+        this.subscriptions.clear();
+    }
+
+    public removeSubscription(key: string) {
+        const sub = this.subscriptions.get(key);
+        sub.unsubscribe();
+    }
+
     public clearTimers() {
         Object.keys(this.timers).forEach((timer) => {
             clearInterval(this.timers[timer]);
--- keel-monitor-app/src/app/simulators/cog.ts
+++ keel-monitor-app/src/app/simulators/cog.ts
@@ -1,6 +1,5 @@
 import { Simulator } from './base-simulator';
 import { Range } from '../models/range';
-import { Observable } from 'rxjs';
 
 export class CourseOverGround extends Simulator {
     heading: number;
@@ -18,75 +17,26 @@ export class CourseOverGround extends Simulator {
             this.stopSimulation();
         }
         this.isSimulationActive = true;
-        this.simulateHeading({ start: 250, end: 290, frequency: 50, delta: 1 });
+        this.simulateHeading({ start: 260, end: 290, frequency: 40, delta: 0.1 });
+        this.simulateCog({ start: 285, end: 295, frequency: 50, delta: 0.1 });
     }
 
     stopSimulation(): void {
-        this.clearTimers();
+        this.clearAllSubscriptions();
     }
 
     private simulateHeading(range: Range) {
         this.heading = range.start;
-        this.foo(range, this.heading).subscribe((val) => {
-            console.log('heading', val);
+        this.addSubscription('heading', this.rangeTicker(range, this.heading).subscribe((val) => {
             this.heading = val;
-        })
-        // this.forwardHeading(range);
+        }));
     }
 
-    private forwardHeading(range: Range) {
-        const forwardClock = setInterval(() => {
-            if (this.heading <= range.end) {
-                this.heading += range.delta;
-            } else {
-                clearInterval(forwardClock);
-                this.backwardHeading(range);
-            }
-        }, range.frequency);
-        this.timers['heading-forward'] = forwardClock;
-    }
-
-    private backwardHeading(range: Range) {
-        const backwardClock = setInterval(() => {
-            if (this.heading >= range.start) {
-                this.heading -= range.delta;
-            } else {
-                clearInterval(backwardClock);
-                this.forwardHeading(range);
-            }
-        }, range.frequency);
-        this.timers['heading-back'] = backwardClock;
-    }
-
-    private foo(range: Range, currentValue: number): Observable<number> {
-        return new Observable((observer) => {
-
-            function forwardClock() {
-                const forward = setInterval(() => {
-                    if (currentValue <= range.end) {
-                        currentValue += range.delta;
-                        observer.next(currentValue);
-                    } else {
-                        clearInterval(forward);
-                        backwardClock()
-                    }
-                }, range.frequency);
-            }
-
-            function backwardClock() {
-                const backward = setInterval(() => {
-                    if (currentValue >= range.start) {
-                        currentValue -= range.delta;
-                        observer.next(currentValue);
-                    } else {
-                        clearInterval(backward);
-                        forwardClock();
-                    }
-                }, range.frequency);
-            }
-
-            forwardClock();
-        });
+    private simulateCog(range: Range) {
+        this.courseOverGround = range.start;
+        this.addSubscription('cog', this.rangeTicker(range, this.courseOverGround).subscribe((val) => {
+            this.courseOverGround = val;
+        }));
     }
 
 }

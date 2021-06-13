41e2969586f7bead84f26437f5aa24a3492e9670
Madhusuthanan-B <madhub081011@gmail.com>
2021-06-13 14:18:08 +0530

    Open bridge integration example

--- keel-monitor-app/src/app/app.module.ts
+++ keel-monitor-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import 'openbridge-web-components';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';

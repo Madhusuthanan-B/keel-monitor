968d0e29c8c793bc1bcfe600f22aa2039c0da134
Madhusuthanan-B <madhub081011@gmail.com>
2021-06-13 13:54:33 +0530

    Sample web component integration

--- keel-monitor-app/src/app/app.component.ts
+++ keel-monitor-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'web-component-essentials';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'keel-monitor-app';
+  open = false;
+
+  toggle(event) {
+    this.open = event.detail;
+  }
 }

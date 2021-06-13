80be69580e1b4308b0216a7be0bb5242d7872193
Madhusuthanan-B <madhub081011@gmail.com>
2021-06-13 17:04:18 +0530

    Simulating azimuth thruster

--- keel-monitor-app/src/app/simulators/azimuth-thruster.ts
+++ keel-monitor-app/src/app/simulators/azimuth-thruster.ts
@@ -17,8 +17,8 @@ export class AzimuthThruster extends Simulator {
         this.angle = 0;
         this.targetAngle = 0;
         this.showPortStarboard = false;
-        this.showSetPointValue = false;
-        this.showSetPointAngle = false;
+        this.showSetPointValue = true;
+        this.showSetPointAngle = true;
     }
 
     startSimulation() {
@@ -27,7 +27,8 @@ export class AzimuthThruster extends Simulator {
         }
         this.isSimulationActive = true;
         this.simulateAngle({ start: 0, end: 360, frequency: 25, delta: 0.1 });
-        this.simulateValue({ start: 22, end: 80, frequency: 50, delta: 1 });
+        this.simulateValue({ start: 20, end: 50, frequency: 50, delta: 1 });
+        this.simulateTargetAngle({ start: 0, end: 360, frequency: 3000, delta: 25 });
     }
 
     stopSimulation() {
@@ -68,5 +69,39 @@ export class AzimuthThruster extends Simulator {
             }
         }, range.frequency);
         this.timers['value'] = timer;
+    }
+
+    private simulateTarget(range: Range) {
+
+        let forward = true;
+        const timer = setInterval(() => {
+
+            if (forward) {
+                this.target = this.target + range.delta;
+                forward = !(this.target > range.end);
+            }
+            else {
+                this.target = this.target - range.delta;
+                forward = this.target === range.start;
+            }
+        }, range.frequency);
+        this.timers['target'] = timer;
+    }
+
+    private simulateTargetAngle(range: Range) {
+
+        let forward = true;
+        const timer = setInterval(() => {
+
+            if (forward) {
+                this.targetAngle = this.targetAngle + range.delta;
+                forward = !(this.targetAngle > range.end);
+            }
+            else {
+                this.targetAngle = this.targetAngle - range.delta;
+                forward = this.targetAngle === range.start;
+            }
+        }, range.frequency);
+        this.timers['targetAngle'] = timer;
     }
 }
